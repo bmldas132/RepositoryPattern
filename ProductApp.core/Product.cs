b0208c1c4ecba00e5b9964ed4684580ee860d7fa
rajeev812 <raj.812@hotmail.com>
2016-08-27 14:21:53 +0530

    Added core

---
+++ ProductApp.core/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace ProductApp.core
+{
+  public class Product
+    {
+      [Key]
+        public int id { get; set; }
+        public int ProdId { get; set; }
+      [Required]
+      [MaxLength(100)]
+        public string ProductName { get; set; }
+      [Required]
+        public double ProductPrice { get; set; }
+        public bool Instock { get; set; }
+    }
+}
